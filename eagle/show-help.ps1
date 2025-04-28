6d33f79079df8d007d0a8071ad1da2e7ab244bfe
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-04-28 22:25:47 +0200

    feat(show-help.ps1): enhance help message with command options and shortcuts

--- eagle/show-help.ps1
+++ eagle/show-help.ps1
@@ -1,9 +1,10 @@
 function Show-Help {
   Write-Host "`nAvailable commands:" -ForegroundColor Yellow
-  Write-Host "  spicetify    : Installs Spicetify" -ForegroundColor Cyan
-  Write-Host "  vencord      : Launches or downloads the Vencord Installer" -ForegroundColor Cyan
-  Write-Host "  update       : Checks for updates to the [at]eagle PS and installs if needed" -ForegroundColor Cyan
-  Write-Host "  uninstall    : Removes [at]eagle PS and cleans up the alias and folder" -ForegroundColor Cyan
-  Write-Host "  version      : Displays the current version of the eagle script" -ForegroundColor Cyan
-  Write-Host "  help         : Displays this help message" -ForegroundColor Cyan
+  Write-Host "  spicetify (--s)    : Installs Spicetify" -ForegroundColor Cyan
+  Write-Host "  vencord (--vencord): Launches or downloads the Vencord Installer" -ForegroundColor Cyan
+  Write-Host "  update (--u)       : Checks for updates to eagle and installs if needed" -ForegroundColor Cyan
+  Write-Host "  uninstall (--rem)  : Removes eagle and cleans up the alias and folder" -ForegroundColor Cyan
+  Write-Host "  version (--v)      : Displays the current version of the eagle script" -ForegroundColor Cyan
+  Write-Host "  help (--h)         : Displays this help message" -ForegroundColor Cyan
+  Write-Host "  apps (--a)         : Updates all applications via winget" -ForegroundColor Cyan
 }
