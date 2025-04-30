33fad44d1d0ac2464d28e051ed3eb49a0d0d09a7
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-04-30 14:33:29 +0200

    fix(show-help.ps1): update vencord command option to use --ven for consistency

--- eagle/show-help.ps1
+++ eagle/show-help.ps1
@@ -1,10 +1,11 @@
 function Show-Help {
   Write-Host "`nAvailable commands:" -ForegroundColor Yellow
-  Write-Host "  spicetify (--s)    : Installs Spicetify" -ForegroundColor Cyan
-  Write-Host "  vencord (--vencord): Launches or downloads the Vencord Installer" -ForegroundColor Cyan
-  Write-Host "  update (--u)       : Checks for updates to eagle and installs if needed" -ForegroundColor Cyan
-  Write-Host "  uninstall (--rem)  : Removes eagle and cleans up the alias and folder" -ForegroundColor Cyan
-  Write-Host "  version (--v)      : Displays the current version of the eagle script" -ForegroundColor Cyan
-  Write-Host "  help (--h)         : Displays this help message" -ForegroundColor Cyan
-  Write-Host "  apps (--a)         : Updates all applications via winget" -ForegroundColor Cyan
+  Write-Host "  spicetify (--s)     : Installs Spicetify" -ForegroundColor Cyan
+  Write-Host "  vencord (--ven)     : Launches or downloads the Vencord Installer" -ForegroundColor Cyan
+  Write-Host "  update (--u)        : Checks for updates to eagle and installs if needed" -ForegroundColor Cyan
+  Write-Host "  uninstall (--rem)   : Removes eagle and cleans up the alias and folder" -ForegroundColor Cyan
+  Write-Host "  version (--v)       : Displays the current version of the eagle script" -ForegroundColor Cyan
+  Write-Host "  help (--h)          : Displays this help message" -ForegroundColor Cyan
+  Write-Host "  apps (--a)          : Updates all applications via winget" -ForegroundColor Cyan
+  Write-Host "  clean (--c)         : Moves unwanted or duplicate project files to the bin folder" -ForegroundColor Cyan
 }
