b8e2c58c94015b6ac80dcdcf41f6c68f688e2e1a
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-05-03 19:23:29 +0200

    refactor: update script version to 2.5.0 and add project creation functionality

--- eagle.ps1
+++ eagle.ps1
@@ -1,34 +1,36 @@
 param (
   [Parameter(Mandatory = $false, Position = 0)]
-  [string]$option = "help"
+  [string]$option = "help",
+  [string]$name,
+  [string]$template
 )
 
-$scriptVersion = "2.4.0"
+$scriptVersion = "2.5.0"
 
 $scriptRoot = $PSScriptRoot
 . "$scriptRoot\eagle\show-help.ps1"
 . "$scriptRoot\eagle\install-spicetify.ps1"
 . "$scriptRoot\eagle\install-vencord.ps1"
-. "$scriptRoot\eagle\update-script.ps1"
 . "$scriptRoot\eagle\uninstall-script.ps1"
 . "$scriptRoot\eagle\show-version.ps1"
+. "$scriptRoot\eagle\create-project.ps1"
 
 $normalized = switch ($option.ToLower()) {
   "--h" { "help" }
   "--v" { "version" }
-  "--u" { "update" }
   "--s" { "spicetify" }
   "--ven" { "vencord" }
   "--rem" { "uninstall" }
+  "--c" { "create" }
   default { $option.ToLower() }
 }
 
 switch ($normalized) {
   "spicetify" { Install-Spicetify }
   "vencord" { Install-Vencord }
-  "update" { Update-Script }
   "uninstall" { Uninstall-Script }
   "version" { Show-Version -Version $scriptVersion }
+  "create" { Install-Project -Name $name -Template $template }
   "help" { Show-Help }
   default {
     Write-Host "❌ Unknown command: '$option'. 'eagle help' for all Commands." -ForegroundColor DarkRed
