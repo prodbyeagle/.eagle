abcac3f34c3a2d6ebc72f4219c9d79433876bd7d
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-06-11 19:12:12 +0200

    feat(Version): Update script version to 2.7.3

--- eagle.ps1
+++ eagle.ps1
@@ -5,7 +5,7 @@ param (
   [string]$template
 )
 
-$scriptVersion = "2.7.1"
+$scriptVersion = "2.7.3"
 
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $coreDir = Join-Path $scriptDir "core"
