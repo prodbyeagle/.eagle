59a10b099603e29be58904c6b30ac2577baade68
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-06-11 19:12:09 +0200

    feat(Help): Update Show-Help function to include new commands and improve formatting

--- core/Show-Help.ps1
+++ core/Show-Help.ps1
@@ -4,8 +4,11 @@ function Show-Help {
   Write-Host "─────────────────────────────────────────────"
 
   $commands = @(
+    @{ Cmd = "eagle"; Alias = "--eagle"; Desc = "Shows eagle animation" },
     @{ Cmd = "spicetify"; Alias = "--s"; Desc = "Installs Spicetify" },
     @{ Cmd = "vencord"; Alias = "--ven"; Desc = "Launches or downloads the Vencord Installer" },
+    @{ Cmd = "vencord:dev"; Alias = "--ven:dev"; Desc = "Launches the Vencord Installer in developer mode" },
+    @{ Cmd = "create"; Alias = "--c"; Desc = "Creates a new project using a template" },
     @{ Cmd = "update"; Alias = "--u"; Desc = "Checks for updates to eagle and installs if needed" },
     @{ Cmd = "uninstall"; Alias = "--rem"; Desc = "Removes eagle and cleans up the alias and folder" },
     @{ Cmd = "version"; Alias = "--v"; Desc = "Displays the current version of the eagle script" },
@@ -13,7 +16,7 @@ function Show-Help {
   )
 
   foreach ($c in $commands) {
-    $line = "{0,-10} {1,-10} : {2}" -f $c.Cmd, "($($c.Alias))", $c.Desc
+    $line = "{0,-15} {1,-12} : {2}" -f $c.Cmd, "($($c.Alias))", $c.Desc
     Write-Host "  $line" -ForegroundColor Blue
   }
 
