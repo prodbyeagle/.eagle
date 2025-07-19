a794690c6c1631a5adad46671f760f49acaca2cb
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-07-19 22:40:17 +0200

    version 2.8.5

--- eagle.ps1
+++ eagle.ps1
@@ -5,7 +5,7 @@ param (
   [string]$template
 )
 
-$scriptVersion = "2.8.1"
+$scriptVersion = "2.8.5"
 
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $coreDir = Join-Path $scriptDir "core"
